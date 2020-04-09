17e882bcaf40718d0e836bbfc83a6eb1056a5602
mechaniclin <mechaniclin@163.com>
2020-04-09 14:16:24 +0800

    modify version

--- build-npm/dist/modules/setting.js
+++ build-npm/dist/modules/setting.js
@@ -53,7 +53,7 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var initialState = {
-    versions: "2.1.15",
+    versions: "2.2.0",
     ops_limit: 100,
     autoReconnect: true,
     defaultSettings: {
