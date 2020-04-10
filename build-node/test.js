5d47c57b9a232f369cf4885db4a449eb2e9aa645
SYLing <534585931@qq.com>
2020-04-10 10:28:52 +0800

    node

--- build-node/test.js
+++ build-node/test.js
@@ -3,15 +3,32 @@ let _bcx=require('./bcx.node.js');
 var http = require("http");
 var url = require("url");
 var querystring = require('querystring')
+// let bcx=_bcx.createBCX({
+//     default_ws_node:"ws://test.cocosbcx.net",
+//     ws_node_list:[	
+//          {url:"ws://test.cocosbcx.net",name:"Cocos - China - Beijing"}   	
+//     ],
+//     networks:[
+//         {
+//             core_asset:"COCOS",
+//             chain_id:"1ae3653a3105800f5722c5bda2b55530d0e9e8654314e2f3dc6d2b010da641c5" 
+//         }
+//     ], 
+//     faucet_url:"",
+//     auto_reconnect:true,
+//     real_sub:true,
+//     check_cached_nodes_data:false                 
+// });
+
 let bcx=_bcx.createBCX({
-    default_ws_node:"ws://test.cocosbcx.net",
+    default_ws_node:"wss://api.cocosbcx.net",
     ws_node_list:[	
-         {url:"ws://test.cocosbcx.net",name:"Cocos - China - Beijing"}   	
+         {url:"wss://api.cocosbcx.net",name:"主网"}   	
     ],
     networks:[
         {
             core_asset:"COCOS",
-            chain_id:"c1ac4bb7bd7d94874a1cb98b39a8a582421d03d022dfa4be8c70567076e03ad0" 
+            chain_id:"6057d856c398875cac2650fe33caef3d5f6b403d184c5154abbff526ec1143c4" 
         }
     ], 
     faucet_url:"",
